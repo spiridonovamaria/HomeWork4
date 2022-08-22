4204f1efcda692160d20037704b590b50d23876d
Maria Spiridonova <traffic2007@mail.ru>
2022-08-22 18:29:13 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+
+
+int Prompt(string message){     
+    System.Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = int.Parse(value);
+    return result;
+}
+
+int SumNumbers(int number){
+    int sum = 0;
+    if(number <1) 
+    number*=-1;
+    while (number >1){
+        sum+= number%10;
+        number /=10;
+    }
+    return sum;
+}
+
+int number = Prompt("Введите число");
+int answer = SumNumbers(number);
+Console.WriteLine(answer);
