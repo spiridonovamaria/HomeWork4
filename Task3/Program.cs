a010d7f574eb7b8b0617075ee3b619776d38a46b
Maria Spiridonova <traffic2007@mail.ru>
2022-08-22 18:37:42 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов, 
+//выводит их на экран. И ищет второй максимум (элемент меньше 
+//максимального, но больше всех остальных)
+
+
+int[] ArrayFill()
+{    
+    System.Console.WriteLine("Массив чисел: ");
+    int[] arr1 = new int[8];
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        arr1[i] = new Random().Next(1, 20);
+    }
+    return arr1;
+}
+
+
+int FindSecondMax(int[] array)
+{
+    int max1 = array[0];
+    int max2 = array[1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= max1)
+        {
+            max2 = max1;
+            max1 = array[i];
+        }
+        if (array[i] > max2 && array[i] < max1)
+        {
+            max2 = array[i];
+        }
+    }
+    return max2;
+}
+
+
+void ArrayPrint(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+//Этапы выполнения задачи
+int[] arr = ArrayFill();
+ArrayPrint(arr);
+int secondMax = FindSecondMax(arr);
+System.Console.WriteLine($"Второе максимальное число: {secondMax}");
